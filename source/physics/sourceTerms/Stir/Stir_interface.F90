6f410ef51870c1051abfac93f4fd3795ce8d2f56
Christoph Federrath <chfeder@TwisterMini.local>
2023-08-29 19:34:14 +1000

    doxygen

--- source/physics/sourceTerms/Stir/Stir_interface.F90
+++ source/physics/sourceTerms/Stir/Stir_interface.F90
@@ -1,7 +1,24 @@
-!!****h* source/physics/sourceTerms/Stir/Stir_interface
+!> @copyright Copyright 2023 UChicago Argonne, LLC and contributors
 !!
-!! Header file for Stir module defining its public interfaces.
-!!***
+!! @licenseblock
+!!   Licensed under the Apache License, Version 2.0 (the "License");
+!!   you may not use this file except in compliance with the License.
+!!
+!!   Unless required by applicable law or agreed to in writing, software
+!!   distributed under the License is distributed on an "AS IS" BASIS,
+!!   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!!   See the License for the specific language governing permissions and
+!!   limitations under the License.
+!! @endlicenseblock
+!!
+!! @file
+!! @brief Public interfaces for the turbulence driving unit Stir
+!!
+!! @details This is the header file for the turbulence driving module Stir
+!!          that defines its public interfaces.
+
+!> @ingroup Stir
+!! Interfaces to Stir public procedures
 Module Stir_interface
 
 #include "constants.h"
