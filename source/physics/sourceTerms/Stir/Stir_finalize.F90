f29407ea0996de15fc5fa33b8cd040c1acc667c3
Christoph Federrath <chfeder@TwisterMini.local>
2023-08-29 19:33:45 +1000

    doxygen

--- source/physics/sourceTerms/Stir/Stir_finalize.F90
+++ source/physics/sourceTerms/Stir/Stir_finalize.F90
@@ -1,15 +1,27 @@
-!!****f* source/physics/sourceTerms/Stir/Stir_finalize
+!> @copyright Copyright 2023 UChicago Argonne, LLC and contributors
 !!
-!! NAME
-!!  Stir_finalize
+!! @licenseblock
+!!   Licensed under the Apache License, Version 2.0 (the "License");
+!!   you may not use this file except in compliance with the License.
 !!
-!! SYNOPSIS
-!!  Stir_finalize()
+!!   Unless required by applicable law or agreed to in writing, software
+!!   distributed under the License is distributed on an "AS IS" BASIS,
+!!   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!!   See the License for the specific language governing permissions and
+!!   limitations under the License.
+!! @endlicenseblock
 !!
-!! DESCRIPTION
-!!  Clean up the Stir unit.
+!! @file
+!! @brief Stir_finalize stub
+
+!> @ingroup Stir
 !!
-!!***
+!! @brief Cleans up turbulence stirring module
+!!
+!! @details
+!! @anchor Stir_finalize_stub
+!!
+!! Cleans up the Stir unit.
 
 subroutine Stir_finalize()
   implicit none
