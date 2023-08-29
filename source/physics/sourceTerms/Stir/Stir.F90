2057f95ada8730d3e0c452c3606785c387c39848
Christoph Federrath <chfeder@TwisterMini.local>
2023-08-29 19:33:25 +1000

    doxygen

--- source/physics/sourceTerms/Stir/Stir.F90
+++ source/physics/sourceTerms/Stir/Stir.F90
@@ -1,19 +1,30 @@
-!!****f* source/physics/sourceTerms/Stir/Stir
+!> @copyright Copyright 2023 UChicago Argonne, LLC and contributors
 !!
-!! NAME
-!!  Stir
+!! @licenseblock
+!!   Licensed under the Apache License, Version 2.0 (the "License");
+!!   you may not use this file except in compliance with the License.
 !!
-!! SYNOPSIS
-!!  Stir(real(IN) :: dt,
-!!       integer(IN), optional :: pass
+!!   Unless required by applicable law or agreed to in writing, software
+!!   distributed under the License is distributed on an "AS IS" BASIS,
+!!   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!!   See the License for the specific language governing permissions and
+!!   limitations under the License.
+!! @endlicenseblock
 !!
-!! DESCRIPTION
-!!  Apply turbulence stirring
+!! @file
+!! @brief Stir stub
+
+!> @ingroup Stir
 !!
-!! ARGUMENTS
-!!  dt : the current timestep
+!! @brief Apply turbulence stirring
 !!
-!!***
+!! @details
+!! @anchor Stir_stub
+!!
+!! Apply turbulence stirring via TurbGen (see Federrath et al. 2010; 2022).
+!!
+!! @param dt    current time step
+!! @param pass  optional in case of split time advancement to indicate 1st or 2nd half step
 
 subroutine Stir(dt, pass)
   implicit none
