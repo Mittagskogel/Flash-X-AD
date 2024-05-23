18f5bf35816ce983e5b7db41b1a421ae46cbf139
christoph.federrath@anu.edu.au <christoph.federrath@anu.edu.au>
2024-05-23 14:53:39 +1000

    analysis script for Stir tests

--- source/Simulation/SimulationMain/Stir/analyse.py
+++ source/Simulation/SimulationMain/Stir/analyse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# written by Christoph Federrath, 2022
+# written by Christoph Federrath, 2022-2024
 
 import argparse
 import numpy as np
@@ -17,9 +17,12 @@ def Gaussian_func(x, mean, sigma):
 # === analyse turbulent field in HDF5 file ===
 def analyse(filename):
 
+    print("Analysing file '"+args.inputfile+"'", color="magenta")
+
     # read data
     gg = fl.FlashGG(filename)
     dirs = ["velx", "vely", "velz"]
+    dirs_label = ["$v_x$", "$v_y$", "$v_z$"]
     dat = []
     for dir in dirs:
         dat.append(gg.GetUniformGrid(dir))
@@ -29,39 +32,33 @@ def analyse(filename):
     # statistics
     mean = np.array([dat[d].mean() for d in range(ncmp)])
     std = np.array([dat[d].std() for d in range(ncmp)])
-    print("mean = ", mean, highlight=1)
-    print("standard deviation = ", std, highlight=1)
+    print("mean velocity = ", mean, highlight=1)
+    print("standard deviation of velocity = ", std, highlight=1)
     # PDF plot
     for d in range(ncmp):
-        pdf, q = cfp.get_pdf(dat[d], bins=min(100,int(0.1*dat[d].size)))
-        cfp.plot(x=q, y=pdf, label="$"+dirs[d]+"$")
+        pdfo = cfp.get_pdf(dat[d], bins=min(100,int(0.1*dat[d].size)))
+        q = pdfo.bin_center
+        pdf = pdfo.pdf
+        cfp.plot(x=q, y=pdf, label=dirs_label[d])
         # fit and plot fit
         print("fit for "+dirs[d]+" component:")
         params = {"mean": [-1.0, 0.0, 1.0], "sigma": [1e-6, 1.0, 1e6]}
         fit_result = cfp.fit(Gaussian_func, q, pdf, params=params)
         xfit = np.linspace(q.min(),q.max(),num=500)
         yfit = Gaussian_func(xfit, *fit_result.popt)
-        cfp.plot(x=xfit, y=yfit, label="$\mathrm{fit:}\:"+dirs[d]+"$")
+        cfp.plot(x=xfit, y=yfit, label="$\mathrm{fit:}\:$"+dirs_label[d])
     # finally, plot everything
-    cfp.plot(xlabel='$q$', ylabel='PDF', ylog=True, save=filename+"_pdf.pdf")
+    cfp.plot(xlabel='$v$', ylabel='PDF', ylog=True, legend_loc="lower left", save=filename+"_pdf.pdf")
 
     # Fourier spectra power comparison
     sp = cfp.get_spectrum(dat, ncmp=ncmp)
-    tot_power = sp["P_tot"].sum()/ncmp
-    print("total power = "+cfp.eform(tot_power), highlight=2)
-    if "P_lgt" in sp:
-        lgt_power = sp["P_lgt"].sum()/ncmp
-        print("logitudinal power = "+cfp.eform(lgt_power)+", relative to total: "+cfp.round(100*lgt_power/tot_power,str_ret=True)+"%", highlight=2)
-    if "P_trv" in sp:
-        trv_power = sp["P_trv"].sum()/ncmp
-        print("transverse power  = "+cfp.eform(trv_power)+", relative to total: "+cfp.round(100*trv_power/tot_power,str_ret=True)+"%", highlight=2)
     # Fourier spectra plot
     k = sp["k"]
     ind = (k > 0) & (sp["P_tot"] > 1e-15)
     cfp.plot(x=k[ind], y=sp["P_tot"][ind], label="total")
-    if "P_lgt" in sp: cfp.plot(x=k[ind], y=sp["P_lgt"][ind], label="long")
-    if "P_trv" in sp: cfp.plot(x=k[ind], y=sp["P_trv"][ind], label="trans")
-    cfp.plot(xlabel='$k$', ylabel='$P(k)$', xlim=[0.9,1.1*np.max(k)], xlog=True, ylog=True, legend_loc='upper right', save=filename+"_spectrum.pdf")
+    if "P_lgt" in sp: cfp.plot(x=k[ind], y=sp["P_lgt"][ind], label="longitudinal")
+    if "P_trv" in sp: cfp.plot(x=k[ind], y=sp["P_trv"][ind], label="transverse")
+    cfp.plot(xlabel='$k$', ylabel='$P_v(k)$', xlim=[0.9,1.1*np.max(k)], xlog=True, ylog=True, legend_loc='upper right', save=filename+"_spectrum.pdf")
 
 # =========================================================
 
@@ -71,11 +68,7 @@ if __name__ == "__main__":
 
     # read arguments
     parser = argparse.ArgumentParser(description='FLASH post-processing analysis script.')
-    parser.add_argument('-i', help="input flash file")
+    parser.add_argument('-i', nargs="?", dest="inputfile", default="Turb_hdf5_plt_cnt_0030", help="input flash file")
     args = parser.parse_args()
 
-    if args.i is None:
-        parser.print_help()
-        exit()
-
-    analyse(args.i)
+    analyse(args.inputfile)
