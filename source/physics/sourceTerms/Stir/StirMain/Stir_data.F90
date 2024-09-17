959dbee2546b9c22f19c8163c159dd7ed5a8d2c0
Christoph Federrath <chfeder@TwisterMini.local>
2024-09-17 22:41:56 +1000

    use of iso_c_binding to deal with real kinds

--- source/physics/sourceTerms/Stir/StirMain/Stir_data.F90
+++ source/physics/sourceTerms/Stir/StirMain/Stir_data.F90
@@ -10,11 +10,13 @@
 !!  Stores the local data for source term: StirMain
 !!
 !! AUTHOR
-!!  Christoph Federrath, 2008-2023
+!!  Christoph Federrath, 2008-2024
 !!
 !!***
 
 Module Stir_data
+
+  use iso_c_binding, ONLY : c_double, c_float
 
   implicit none
 
@@ -24,9 +26,9 @@ Module Stir_data
   character (len=80), save :: st_infilename
   logical, save  :: st_useStir, st_computeDt
   real, save :: st_stop_driving_time
-  real(kind=8), save :: dt_update_accel
+  real(c_double), save :: dt_update_accel
 
-  ! local container of real kind=4 to receive the turbulent acceleration field
-  real(kind=4), save, dimension(NXB, NYB, NZB) :: accx, accy, accz
+  ! local container of real kind c_float to receive the turbulent acceleration field
+  real(c_float), save, dimension(NXB, NYB, NZB) :: accx, accy, accz
 
 end Module Stir_data
