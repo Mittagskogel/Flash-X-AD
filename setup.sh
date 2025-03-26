ad27f1c697055215cf08adae6d980cbc7548114e
Faveo Hörold <faveo.hoerold@cscs.ch>
2025-03-26 14:58:58 +0100

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#ml hypre/2.31.0-gcc-11.4.1-2kjgx3l hdf5/1.14.3-gcc-11.4.1-enj3ujb
+# ml hdf5/1.14.3-gcc-11.4.1-enj3ujb
+
+export PATH=/scratch/fhrold/riken/openmpi-5.0.6-install/bin/:/scratch/fhrold/riken/Flash-X/bin/:$PATH
+export LD_LIBRARY_PATH=/scratch/fhrold/riken/ma28-install/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/scratch/fhrold/riken/hdf5-1.14.6-install/lib/:$LD_LIBRARY_PATH
