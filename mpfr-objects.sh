3d096931b49c621733863ab17005d2294b32b050
Faveo Hörold <faveo.hoerold@cscs.ch>
2025-04-24 15:23:48 +0200

    Remove debug flag from mpfr-objects.sh

--- mpfr-objects.sh
+++ mpfr-objects.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 cp ${BASE_PATH}/Enzyme/enzyme/include/enzyme/fprt/mpfr.h "./mpfr.cpp"
-clang++ -c -g "./mpfr.cpp" $(pkg-config --cflags mpfr gmp) \
+clang++ -c "./mpfr.cpp" $(pkg-config --cflags mpfr gmp) \
     -I${BASE_PATH}/Enzyme/enzyme/include/enzyme/fprt/ \
     -DENZYME_FPRT_ENABLE_GARBAGE_COLLECTION \
     -DENZYME_FPRT_ENABLE_SHADOW_RESIDUALS \
