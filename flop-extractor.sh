a92597ddd9d96640fcb6d2ffdb969eabf531f964
Faveo Hörold <faveo.hoerold@cscs.ch>
2025-04-27 21:59:07 +0200

    Output all flop counters in flop-extractor.sh

--- flop-extractor.sh
+++ flop-extractor.sh
@@ -4,21 +4,33 @@ set -e
 set -x
 
 offsets=(0 1 2 3)
-mantissas=($(seq 4 1 52))
+mantissas=($(seq 5 1 53))
 steps=(0001)
 
-rundir=autorun.sedov.new
+rundir=autorun.sod.exponent.fast
+# rundir=autorun.sedov.exponent.fast
+# rundir=autorun.cellular.hydro.restart
+# rundir=autorun.cellular.burn.lscount
+# rundir=autorun.cellular.both.lscount
+# rundir=autorun.risingbubble.advection.re3500.restart.new
 
 for step in ${steps[@]}
 do
     for offset in ${offsets[@]}
     do
-        outfile="${rundir}/flop_counts_ref${offset}_${step}.out"
+        outfile="${rundir}/perf_counts_ref${offset}_${step}.out"
         rm -f ${outfile}
-        echo -n "mantissa" >> ${outfile}
 
-        echo -n " dens-${offset}-trunc" >> ${outfile}
-        echo    " dens-${offset}-double" >> ${outfile}
+        echo "mantissa" \
+             " dens-${offset}-trunc"  \
+             " dens-${offset}-double" \
+             " dens-${offset}-float"  \
+             " dens-${offset}-half"   \
+             " dens-${offset}-trunc-store" \
+             " dens-${offset}-trunc-load" \
+             " dens-${offset}-original-store" \
+             " dens-${offset}-original-load" \
+             >> ${outfile}
 
         for mantissa in ${mantissas[@]}
         do
@@ -28,9 +40,33 @@ do
             objdir=${rundir}/object_${id}
 
             echo -n " "$(grep "Number of truncated flops:" ${objdir}/run_raw.log | \
-                sed 's/Number of truncated flops://') >> ${outfile}
-            echo    " "$(grep "Number of double flops:" ${objdir}/run_raw.log | \
-                sed 's/Number of double flops://') >> ${outfile}
+                sed 's/Number of truncated flops://' | \
+                paste -sd'+' | bc) >> ${outfile}
+
+            echo -n " "$(grep "Number of double flops:" ${objdir}/run_raw.log | \
+                sed 's/Number of double flops://' | \
+                paste -sd'+' | bc) >> ${outfile}
+            echo -n " "$(grep "Number of float flops:" ${objdir}/run_raw.log | \
+                sed 's/Number of float flops://' | \
+                paste -sd'+' | bc) >> ${outfile}
+            echo -n " "$(grep "Number of half flops:" ${objdir}/run_raw.log | \
+                sed 's/Number of half flops://' | \
+                paste -sd'+' | bc) >> ${outfile}
+
+            echo -n " "$(grep "Number of truncated stores:" ${objdir}/run_raw.log | \
+                sed 's/Number of truncated stores://' | \
+                paste -sd'+' | bc) >> ${outfile}
+            echo -n " "$(grep "Number of truncated loads:" ${objdir}/run_raw.log | \
+                sed 's/Number of truncated loads://' | \
+                paste -sd'+' | bc) >> ${outfile}
+            echo -n " "$(grep "Number of original stores:" ${objdir}/run_raw.log | \
+                sed 's/Number of original stores://' | \
+                paste -sd'+' | bc) >> ${outfile}
+            echo -n " "$(grep "Number of original loads:" ${objdir}/run_raw.log | \
+                sed 's/Number of original loads://' | \
+                paste -sd'+' | bc) >> ${outfile}
+
+            echo >> ${outfile}
         done
     done
 done
