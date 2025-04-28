65c1279ac4eac650431cfa396504d45bcfe9f5f9
Faveo Hörold <faveo.hoerold@cscs.ch>
2025-04-28 11:37:36 +0200

    New postprocessing scripts for Sedov and Sod.

--- pp-sedov.sh
+++ pp-sedov.sh
@@ -1,25 +1,32 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
-set -x
 
+# List of offsets for AMR levels
 offsets=(0 1 2 3)
+# List of mantissas
 mantissas=($(seq 5 1 53))
-steps=(0001)
+# List of checkpoint timesteps
+steps=($(seq -w 0001 0001))
 
-rundir=autorun.sod.exponent.fast
-# rundir=autorun.sedov.exponent.fast
-# rundir=autorun.cellular.hydro.restart
-# rundir=autorun.cellular.burn.lscount
-# rundir=autorun.cellular.both.lscount
-# rundir=autorun.risingbubble.advection.re3500.restart.new
+# Directory for automatic experiment runner
+rundir=autorun.sedov
+# Directory for the reference solution
+references=sedov_reference
+
+
 
 for step in ${steps[@]}
 do
+    echo "step ${step}:"
     for offset in ${offsets[@]}
     do
-        outfile="${rundir}/perf_counts_ref${offset}_${step}.out"
-        rm -f ${outfile}
+        echo -n "offset ${offset}:"
+
+        outfile_sfocu="${rundir}/sfocu_ref${offset}_${step}.out"
+        outfile_flops="${rundir}/perf_counts_ref${offset}_${step}.out"
+        rm -f ${outfile_sfocu} ${outfile_flops}
+        touch ${outfile_sfocu} ${outfile_flops}
 
         echo "mantissa" \
              " dens-${offset}-trunc"  \
@@ -30,43 +37,52 @@ do
              " dens-${offset}-trunc-load" \
              " dens-${offset}-original-store" \
              " dens-${offset}-original-load" \
-             >> ${outfile}
+             >> ${outfile_flops}
 
         for mantissa in ${mantissas[@]}
         do
-            echo -n "${mantissa}" >> ${outfile}
+            echo -n " ${mantissa}"
 
             id=ref${offset}_${mantissa}bit
             objdir=${rundir}/object_${id}
 
+            # Get L1-Error >> sfocu_ref*.out
+            sfocu ${objdir}/flashx_hdf5_chk_${step} \
+                ${reference}/flashx_hdf5_chk_${step} | \
+                grep -v "Couldn't read" >> \
+                ${outfile_sfocu} || true
+
+            # Read flop counters >> perf_counts_ref*.out
             echo -n " "$(grep "Number of truncated flops:" ${objdir}/run_raw.log | \
                 sed 's/Number of truncated flops://' | \
-                paste -sd'+' | bc) >> ${outfile}
+                paste -sd'+' | bc) >> ${outfile_flops}
 
             echo -n " "$(grep "Number of double flops:" ${objdir}/run_raw.log | \
                 sed 's/Number of double flops://' | \
-                paste -sd'+' | bc) >> ${outfile}
+                paste -sd'+' | bc) >> ${outfile_flops}
             echo -n " "$(grep "Number of float flops:" ${objdir}/run_raw.log | \
                 sed 's/Number of float flops://' | \
-                paste -sd'+' | bc) >> ${outfile}
+                paste -sd'+' | bc) >> ${outfile_flops}
             echo -n " "$(grep "Number of half flops:" ${objdir}/run_raw.log | \
                 sed 's/Number of half flops://' | \
-                paste -sd'+' | bc) >> ${outfile}
+                paste -sd'+' | bc) >> ${outfile_flops}
 
             echo -n " "$(grep "Number of truncated stores:" ${objdir}/run_raw.log | \
                 sed 's/Number of truncated stores://' | \
-                paste -sd'+' | bc) >> ${outfile}
+                paste -sd'+' | bc) >> ${outfile_flops}
             echo -n " "$(grep "Number of truncated loads:" ${objdir}/run_raw.log | \
                 sed 's/Number of truncated loads://' | \
-                paste -sd'+' | bc) >> ${outfile}
+                paste -sd'+' | bc) >> ${outfile_flops}
             echo -n " "$(grep "Number of original stores:" ${objdir}/run_raw.log | \
                 sed 's/Number of original stores://' | \
-                paste -sd'+' | bc) >> ${outfile}
+                paste -sd'+' | bc) >> ${outfile_flops}
             echo -n " "$(grep "Number of original loads:" ${objdir}/run_raw.log | \
                 sed 's/Number of original loads://' | \
-                paste -sd'+' | bc) >> ${outfile}
+                paste -sd'+' | bc) >> ${outfile_flops}
 
-            echo >> ${outfile}
+            echo >> ${outfile_flops}
+
+            echo
         done
     done
 done
---
+++ pp-sod.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+set -e
+
+# List of offsets for AMR levels
+offsets=(0 1 2)
+# List of mantissas
+mantissas=($(seq 5 1 53))
+# List of checkpoint timesteps
+steps=($(seq -w 0001 0001))
+
+# Directory for automatic experiment runner
+rundir=autorun.sod
+# Directory for the reference solution
+references=sod_reference
+
+
+
+for step in ${steps[@]}
+do
+    echo "step ${step}:"
+    for offset in ${offsets[@]}
+    do
+        echo -n "offset ${offset}:"
+
+        outfile_sfocu="${rundir}/sfocu_ref${offset}_${step}.out"
+        outfile_flops="${rundir}/perf_counts_ref${offset}_${step}.out"
+        rm -f ${outfile_sfocu} ${outfile_flops}
+        touch ${outfile_sfocu} ${outfile_flops}
+
+        echo "mantissa" \
+             " dens-${offset}-trunc"  \
+             " dens-${offset}-double" \
+             " dens-${offset}-float"  \
+             " dens-${offset}-half"   \
+             " dens-${offset}-trunc-store" \
+             " dens-${offset}-trunc-load" \
+             " dens-${offset}-original-store" \
+             " dens-${offset}-original-load" \
+             >> ${outfile_flops}
+
+        for mantissa in ${mantissas[@]}
+        do
+            echo -n " ${mantissa}"
+
+            id=ref${offset}_${mantissa}bit
+            objdir=${rundir}/object_${id}
+
+            # Get L1-Error >> sfocu_ref*.out
+            sfocu ${objdir}/flashx_hdf5_chk_${step} \
+                ${reference}/flashx_hdf5_chk_${step} | \
+                grep -v "Couldn't read" >> \
+                ${outfile_sfocu} || true
+
+            # Read flop counters >> perf_counts_ref*.out
+            echo -n " "$(grep "Number of truncated flops:" ${objdir}/run_raw.log | \
+                sed 's/Number of truncated flops://' | \
+                paste -sd'+' | bc) >> ${outfile_flops}
+
+            echo -n " "$(grep "Number of double flops:" ${objdir}/run_raw.log | \
+                sed 's/Number of double flops://' | \
+                paste -sd'+' | bc) >> ${outfile_flops}
+            echo -n " "$(grep "Number of float flops:" ${objdir}/run_raw.log | \
+                sed 's/Number of float flops://' | \
+                paste -sd'+' | bc) >> ${outfile_flops}
+            echo -n " "$(grep "Number of half flops:" ${objdir}/run_raw.log | \
+                sed 's/Number of half flops://' | \
+                paste -sd'+' | bc) >> ${outfile_flops}
+
+            echo -n " "$(grep "Number of truncated stores:" ${objdir}/run_raw.log | \
+                sed 's/Number of truncated stores://' | \
+                paste -sd'+' | bc) >> ${outfile_flops}
+            echo -n " "$(grep "Number of truncated loads:" ${objdir}/run_raw.log | \
+                sed 's/Number of truncated loads://' | \
+                paste -sd'+' | bc) >> ${outfile_flops}
+            echo -n " "$(grep "Number of original stores:" ${objdir}/run_raw.log | \
+                sed 's/Number of original stores://' | \
+                paste -sd'+' | bc) >> ${outfile_flops}
+            echo -n " "$(grep "Number of original loads:" ${objdir}/run_raw.log | \
+                sed 's/Number of original loads://' | \
+                paste -sd'+' | bc) >> ${outfile_flops}
+
+            echo >> ${outfile_flops}
+
+            echo
+        done
+    done
+done
