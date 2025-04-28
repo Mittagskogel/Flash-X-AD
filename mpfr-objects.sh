05fac9048d0a214d225a2149b4a5ac6cbc987ba9
Faveo Hörold <faveo.hoerold@cscs.ch>
2025-04-28 17:22:27 +0200

    Correct paths.

--- mpfr-objects.sh
+++ mpfr-objects.sh
@@ -3,6 +3,4 @@
 cp ${BASE_PATH}/Enzyme/enzyme/include/enzyme/fprt/mpfr.h "./mpfr.cpp"
 clang++ -c "./mpfr.cpp" $(pkg-config --cflags mpfr gmp) \
     -I${BASE_PATH}/Enzyme/enzyme/include/enzyme/fprt/ \
-    -DENZYME_FPRT_ENABLE_GARBAGE_COLLECTION \
-    -DENZYME_FPRT_ENABLE_SHADOW_RESIDUALS \
     -o "./mpfr.o"
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export PATH=${BASE_PATH}/openmpi-5.0.6-install/bin/:/scratch/fhrold/riken/Flash-X/bin/:$PATH
+export PATH=${BASE_PATH}/openmpi-5.0.6-install/bin/:${BASE_PATH}/Flash-X/bin/:$PATH
 export CPATH=${BASE_PATH}/openmpi-5.0.6-install/include/:$CPATH
 export LD_LIBRARY_PATH=${BASE_PATH}/ma28-install/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=${BASE_PATH}/hdf5-1.14.6-install/lib/:$LD_LIBRARY_PATH
