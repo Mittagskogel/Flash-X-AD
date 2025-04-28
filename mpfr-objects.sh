7bf27963cefc332ce92ccc61895916ea06228f52
Faveo Hörold <faveo.hoerold@cscs.ch>
2025-04-28 15:09:40 +0200

    Update util scripts.

--- mpfr-objects.sh
+++ mpfr-objects.sh
@@ -6,7 +6,3 @@ clang++ -c "./mpfr.cpp" $(pkg-config --cflags mpfr gmp) \
     -DENZYME_FPRT_ENABLE_GARBAGE_COLLECTION \
     -DENZYME_FPRT_ENABLE_SHADOW_RESIDUALS \
     -o "./mpfr.o"
-
-    # -DENZYME_FPRT_ENABLE_GARBAGE_COLLECTION \
-# clang++ -c "./fprt.cpp" $(pkg-config --cflags mpfr gmp) \
-#     -I/scratch/fhrold/riken/Enzyme/enzyme/include/enzyme/fprt/ -o "./fprt.o"
--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
-#ml hypre/2.31.0-gcc-11.4.1-2kjgx3l hdf5/1.14.3-gcc-11.4.1-enj3ujb
-# ml hdf5/1.14.3-gcc-11.4.1-enj3ujb
-
-export BASE_DIR=/scratch/fhrold/riken
-export PATH=${BASE_DIR}/openmpi-5.0.6-install/bin/:/scratch/fhrold/riken/Flash-X/bin/:$PATH
-export CPATH=${BASE_DIR}/openmpi-5.0.6-install/include/:$CPATH
-export LD_LIBRARY_PATH=${BASE_DIR}/ma28-install/lib/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=${BASE_DIR}/hdf5-1.14.6-install/lib/:$LD_LIBRARY_PATH
+export PATH=${BASE_PATH}/openmpi-5.0.6-install/bin/:/scratch/fhrold/riken/Flash-X/bin/:$PATH
+export CPATH=${BASE_PATH}/openmpi-5.0.6-install/include/:$CPATH
+export LD_LIBRARY_PATH=${BASE_PATH}/ma28-install/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${BASE_PATH}/hdf5-1.14.6-install/lib/:$LD_LIBRARY_PATH
